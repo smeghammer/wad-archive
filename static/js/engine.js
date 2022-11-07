90c72673c3c430f7338a4781fd6a8846de51a9c4
Silas <silas@thecoven.co.uk>
2022-11-07 22:16:24 +0000

    added basic pagination and a CSS stub.

--- static/js/engine.js
+++ static/js/engine.js
@@ -29,12 +29,15 @@ let engine = {
     https://stackoverflow.com/questions/50776445/vanilla-javascript-version-of-ajax
   });
 	 */
-	loadFiles: function(){
-
+	loadFiles: function(pageNum){
+		pNum = 0;
+		if(pageNum){
+			pNum = pageNum;
+		}
 		/** look for pagination flags */
 		let pageSize = document.getElementById('body').getAttribute('data-page-size');
 		console.log(pageSize);
-		fetch('/app/files/'+pageSize + '/0')
+		fetch('/app/files/'+pageSize + '/' + pNum)
 		.then(function(response){
 			/** NOTE: return the filecount as part of the pagination response data! */
 			data = response.json();
@@ -47,17 +50,49 @@ let engine = {
 		})
 	},
 	
+	//http://127.0.0.1:5000/app/files/20/0
 	buildPaginator : function(currentPage, pageSize, recordCount){
-		console.log(currentPage, pageSize, recordCount);
 		
-		let num_pages = math.ceil(recordCount/pageSize);
+		let num_pages = Math.ceil(recordCount/pageSize);
+		let paginator = document.createElement('div');
+		let prevtag = document.createElement('a');
+		let nexttag = document.createElement('a');
+		let summary_tag = document.createElement('span');
+		summary_tag.appendChild(document.createTextNode('Page ' + currentPage + ' of '+num_pages))
+		prevtag.setAttribute('class','paginator_prev');
+		nexttag.setAttribute('class','paginator_next');
+		prevtag.setAttribute('id','paginator_prev');
+		nexttag.setAttribute('id','paginator_next');
+		prevtag.appendChild(document.createTextNode(' < '));
+		nexttag.appendChild(document.createTextNode(' > '));
+		
+		console.log(currentPage, pageSize,num_pages, recordCount);
 		if(currentPage < num_pages){
 			// make next link
 		}
 		if(currentPage > 0){
 			// make prev link
 		}
+		//https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/
+		prevtag.addEventListener('click',function(){
+			console.log('clicked prev')
+			engine.loadFiles((parseInt(currentPage) - 1));
+		});
+		nexttag.addEventListener('click',function(){
+			console.log('clicked next');
+			engine.loadFiles((parseInt(currentPage) + 1));
+		});
 		
+		paginator.appendChild(prevtag);
+		paginator.appendChild(summary_tag);
+		paginator.appendChild(nexttag);
+		
+		/** append click handlers */
+		
+		
+		let elem = document.getElementById('paginator');
+		elem.innerHTML = "";
+		elem.appendChild(paginator);
 	},
 	
 	buildFileLinks : function(fileData){
