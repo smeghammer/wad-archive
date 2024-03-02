1a1699b745f3ad693e08fef1b6228bcee3433a70
Smeghammer <smeghammer@live.com>
2024-03-02 12:16:12 +0000

    #11\n\nfirsc commt for NFS addition - WIP

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,10 @@
 settings = {
-    'archive_root_path' : '/media/silas/Seagate Expansion Drive/wad-archive-dump/DATA',
+    # 'archive_root_path' : '/media/silas/Seagate Expansion Drive/wad-archive-dump/DATA',
+    'archive_root_path' : 'nfs://192.168.1.20/mnt/USB/USB1_c2/wad-archive-dump',
     'records_per_page' : 40,
     'metadata_database_name' : 'wadarchive',
     'metadata_database_address' : '192.168.1.115',
     'metadata_database_port' : 27017
 }
+
+# nfs://192.168.1.20/mnt/USB/USB1_c2/wad-archive-dump
